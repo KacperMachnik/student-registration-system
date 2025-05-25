a32485aeada028bcdc8618b3a8da411feb5819d5
Patryk Przybysz <PatrykPrzybysz.tl@interia.pl>
2025-05-25 12:18:40 +0200

    basic interaction with fe + docker

--- src/main/java/pl/edu/agh/student_registration_system/security/WebSecurityConfig.java
+++ src/main/java/pl/edu/agh/student_registration_system/security/WebSecurityConfig.java
@@ -20,6 +20,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import pl.edu.agh.student_registration_system.model.*;
 import pl.edu.agh.student_registration_system.repository.RoleRepository;
 import pl.edu.agh.student_registration_system.repository.StudentRepository;
@@ -28,6 +31,7 @@ import pl.edu.agh.student_registration_system.security.jwt.AuthEntryPointJwt;
 import pl.edu.agh.student_registration_system.security.jwt.AuthTokenFilter;
 import pl.edu.agh.student_registration_system.security.jwt.JwtUtils;
 import pl.edu.agh.student_registration_system.security.service.UserDetailsServiceImpl;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -78,6 +82,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -137,6 +142,18 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     CommandLineRunner initData(RoleRepository roleRepository,
